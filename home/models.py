bb519cf3e6b977a6cf4aa3ee4bb00766d33bd86c
telegram_bot <boxodirazimov683@gamil.com>
2024-05-20 12:34:51 +0500

    Normalize all the line endings to LF

---
+++ home/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    last_updated = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class PriceType(models.TextChoices):
+    EURO = 'EURO', 'EURO'
+    DOLLAR = '$', '$'
+    SUM = 'SO`M', 'SO`M'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.FloatField()
+    description = models.TextField()
+    number = models.IntegerField()
+    image = models.ImageField(upload_to='media/product')
+    price_type = models.CharField(max_length=50, choices=PriceType.choices)
+    last_updated = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    rating = models.FloatField(max_length=1, default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class Profile(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='media/profile')
+    address = models.TextField()
+    last_updated = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.first_name
+
+
+class Client(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    rating = models.FloatField(max_length=1, default=0)
+    comment = models.TextField()
+    profession = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='media/client')
+
+    def __str__(self):
+        return self.first_name
